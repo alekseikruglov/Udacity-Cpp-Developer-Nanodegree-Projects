75ecf2f2af2e3ecfa29861d1a15e92aad92b538d
as <you@example.com>
2021-02-25 20:11:23 +0100

    Fix typos

--- Project_5_Cpastone-Project_Car-Counter/build/lib/pugixml/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/as/udacity_c++/Project5/CppND-Capstone-Hello-World/lib/pugixml
-# Build directory: /home/as/udacity_c++/Project5/CppND-Capstone-Hello-World/build/lib/pugixml
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
